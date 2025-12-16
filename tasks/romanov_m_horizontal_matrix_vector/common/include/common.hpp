87a5e9d7447436b340eebd47f7dd3d239372eee2
unknown <vonamorm@mail.ru>
2025-12-17 01:53:10 +0300

    Итоговый вариант работы

--- tasks/romanov_m_horizontal_matrix_vector/common/include/common.hpp
+++ tasks/romanov_m_horizontal_matrix_vector/common/include/common.hpp
@@ -9,7 +9,7 @@ namespace romanov_m_horizontal_matrix_vector {
 
 using InType = std::tuple<std::vector<double>, int, int, std::vector<double>>;
 using OutType = std::vector<double>;
-using TestType = std::tuple<int, int>;  
+using TestType = std::tuple<int, int>;
 using BaseTask = ppc::task::Task<InType, OutType>;
 
 }  // namespace romanov_m_horizontal_matrix_vector
